5db2b465c22b3f134ffa7d963f91c801ad476283
Sadie Cutts <113192692+Sadie3874@users.noreply.github.com>
2025-06-08 17:48:06 -0500

    Update AIPromptStringBuilder.cs

--- TheCoAuthorsMaster/The CoAuthors/Controllers/AIPromptStringBuilder.cs
+++ TheCoAuthorsMaster/The CoAuthors/Controllers/AIPromptStringBuilder.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -35,9 +35,21 @@ namespace The_CoAuthors.Controllers
             "called in. This is a murder mystery, so we need to figure out the murderer. Don’t make " +
             "it obvious, but Lucy is the murderer — make her seem completely shaken and innocent.",
 
-            "NPC2",
+            "You are Leo. Leo is a cleaner that comes in every week to clean and get groceries for the house. " +
+            "Leo is in his mid 20s and has been employed for about a year now. He is hard working and takes " +
+            "feedback every well. Leo came to work around the same time lucy did on the morning of the discovery. " +
+            "He opened the door to find the house a huge mess. For Leo this is normal to find the house a mess, " +
+            "he figures that it is just cause of oldness. Leo started to clean the kitchen first, cause lucy needed " +
+            "it to make breakfast, then he went to the washrooms and tidied those up to. Then he heard the scream. " +
+            "Lucy came running down the stairs screaming “she’s dead!”. Leo says it happened fast that he doesn’t " +
+            "remember much but will help the best he can.  ",
 
-            "NPC3"
+            "You are Fred. Fred is the butler of the house. He manages all the grandma’s bank details along with " +
+            "paying the employees and keeping the house in shape. Fred fell sick the day of the discovery, " +
+            "so he didn’t come into work. Fred says as soon as he got the call he came into work. " +
+            "Fred has been working for the grandma for 10 years now and many other people have stated that he " +
+            "is the most trustworthy and has been the closest to the grandma ever since the incident 10 years ago. " +
+            "Fred is heart broken from the loss of his friend but will try his best to help with the investigation."
         };
 
         public List<string> RoomNames = new List<string>()
@@ -49,18 +61,34 @@ namespace The_CoAuthors.Controllers
             "the room, or anything else that fits in the storyline, such as details about the grandma, " +
             "who you are to the grandma, etc. Not every response should be a clue!",
 
-            "Stairway",
+            "Stairway. This is the second room the character will enter. " +
+            "Here is a creepy hallway filled with torn pages from books. " +
+            "The player will find 3 clues here, a strange bottle, blood and " +
+            "some book pages. The bottle can be found on the small table by the stairs. " +
+            "This bottle contains some kind of red liquid and has strange markings on the lid. " +
+            "The next item the player find is the blood the blood is in front of the stairs " +
+            "on the left side of the picture. We don’t know whose blood this is. " +
+            "The final clue is the torn pages. These pages show strange creatures and odd markings; " +
+            "the player cannot decipher them. These pages are ancient but have been left in pristine condition",
 
-            "Summoning Circle"
+            "Bed Room. This is the final room the player visits. " +
+            "This room has a lot of scratch marks and is the room where the grandma’s dead body was found. " +
+            "In the room the player will find 3 clues. A knife, a picture frame and a news paper. " +
+            "The knife can be found in front of the bed, it is left out in the open, almost like someone wanted" +
+            " us to find it easily. This knife has blood on it but no fingerprints. The second item is the picture frame. " +
+            "This is a photo of a mysterious person, you don’t know who she is, but the player suspects she was the " +
+            "granddaughter of the grandma. The final clue is the news paper. This news paper contains the details of" +
+            " the incident from 10 years ago. The incident is the disappearance of the grandmother’s granddaughter; " +
+            "her body was found by the river just outside of town. "
         };
 
         public List<List<string>> ItemsInRooms = new List<List<string>>()
         {
             new List<string> { "key", "glasses", "necklace", "book" },
 
-            new List<string> { },
+            new List<string> { "bottle", "blood", "torn pages"},
 
-            new List<string> { }
+            new List<string> { "knife", "pictureframe", "newspaper"}
         };
 
         // AI
